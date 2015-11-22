9d55b51b8aabbf0946c1a5aa9f066bbf828cd382
Łukasz Śliwa <lmsliwa@gmail.com>
2015-11-22 14:51:53 +0100

    initial commit: setup project, add pods

---
+++ ExampleYoutubePlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExampleYoutubePlayer
+//
+//  Created by Łukasz Śliwa on 22/11/15.
+//  Copyright © 2015 Łukasz Śliwa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
